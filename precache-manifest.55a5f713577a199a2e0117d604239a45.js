9f8636c625724352695682b6599fe5c41362d3d0
Efrain Jared Ramirez Diaz <efrain_jared@hotmail.com>
2020-11-12 15:05:19 -0600

    Updates

---
+++ precache-manifest.55a5f713577a199a2e0117d604239a45.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "789d86b4647481fb56133b872e674349",
+    "url": "/JaredRamirezDiaz.github.io/index.html"
+  },
+  {
+    "revision": "1a0a6f11af975b410a4d",
+    "url": "/JaredRamirezDiaz.github.io/static/css/2.98705a83.chunk.css"
+  },
+  {
+    "revision": "f0f438125200c9ea8302",
+    "url": "/JaredRamirezDiaz.github.io/static/css/main.8dbf41d3.chunk.css"
+  },
+  {
+    "revision": "1a0a6f11af975b410a4d",
+    "url": "/JaredRamirezDiaz.github.io/static/js/2.a17cd977.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/JaredRamirezDiaz.github.io/static/js/2.a17cd977.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0f438125200c9ea8302",
+    "url": "/JaredRamirezDiaz.github.io/static/js/main.243d59cc.chunk.js"
+  },
+  {
+    "revision": "ebf1eb18932d82cce61f",
+    "url": "/JaredRamirezDiaz.github.io/static/js/runtime-main.d65857ab.js"
+  },
+  {
+    "revision": "6e3271fe3bbd9f7911be39f6ce4c6232",
+    "url": "/JaredRamirezDiaz.github.io/static/media/ALaFinalTrompeta.6e3271fe.jpg"
+  },
+  {
+    "revision": "44b350ba0b7974833c21c002723dd564",
+    "url": "/JaredRamirezDiaz.github.io/static/media/AnclaDeSalvacionImprimir.44b350ba.jpg"
+  },
+  {
+    "revision": "9fd7601a583e5ca11d094d6e02524edf",
+    "url": "/JaredRamirezDiaz.github.io/static/media/AvatarWallpaper.9fd7601a.jpg"
+  },
+  {
+    "revision": "05d1fd61b275d28949847e7b6ea71ab5",
+    "url": "/JaredRamirezDiaz.github.io/static/media/Concierto-Banner_2.05d1fd61.gif"
+  },
+  {
+    "revision": "dc2d98eff11d47d50eb552d897f298f9",
+    "url": "/JaredRamirezDiaz.github.io/static/media/EfectoPermanencia.dc2d98ef.jpg"
+  },
+  {
+    "revision": "27161efdc095914249d736c19e6eece9",
+    "url": "/JaredRamirezDiaz.github.io/static/media/FotoCV.27161efd.jpg"
+  },
+  {
+    "revision": "1b340a9c9c718c2043d2363b596081e6",
+    "url": "/JaredRamirezDiaz.github.io/static/media/Heroes-de-la-biblia.1b340a9c.jpg"
+  },
+  {
+    "revision": "82362de5ac90006f0bf4c652763b186c",
+    "url": "/JaredRamirezDiaz.github.io/static/media/ItsurGeekWallpapperBW.82362de5.jpg"
+  },
+  {
+    "revision": "89ee8a58531f073a49cd04f6e99c3da6",
+    "url": "/JaredRamirezDiaz.github.io/static/media/LonaV.89ee8a58.jpg"
+  },
+  {
+    "revision": "561213b6abff7621dedb9f4eaf7e3580",
+    "url": "/JaredRamirezDiaz.github.io/static/media/PosterAncla.561213b6.jpg"
+  },
+  {
+    "revision": "27157ede54ec6463962fafdc76af9d7b",
+    "url": "/JaredRamirezDiaz.github.io/static/media/PosterEnsamble.27157ede.jpg"
+  },
+  {
+    "revision": "c1109298ce4b37eb517c7ba70d0e0dc6",
+    "url": "/JaredRamirezDiaz.github.io/static/media/PosterNocheMexicana.c1109298.jpg"
+  },
+  {
+    "revision": "47c5d45bdd183abefda9b26fa07c2e01",
+    "url": "/JaredRamirezDiaz.github.io/static/media/TheLionAndTheLamb.47c5d45b.jpg"
+  },
+  {
+    "revision": "63a963a85029e0701f6369647dd2e9a5",
+    "url": "/JaredRamirezDiaz.github.io/static/media/WallpaperItsurGeek.63a963a8.jpg"
+  },
+  {
+    "revision": "28f7d6fa8102adf57e04c5aca98e1f02",
+    "url": "/JaredRamirezDiaz.github.io/static/media/avatar.28f7d6fa.svg"
+  },
+  {
+    "revision": "3dfa83d4ba7e5679b3c5c4e747751a78",
+    "url": "/JaredRamirezDiaz.github.io/static/media/navbar-first_plane.3dfa83d4.png"
+  },
+  {
+    "revision": "4aac1c0d03eacc64a7d67e4155a42f24",
+    "url": "/JaredRamirezDiaz.github.io/static/media/navbar-second_plane.4aac1c0d.png"
+  },
+  {
+    "revision": "f0f6d6ffd03f2ab7adbdd24d183474a8",
+    "url": "/JaredRamirezDiaz.github.io/static/media/perfil2.f0f6d6ff.jpg"
+  },
+  {
+    "revision": "e2022059cee4e34b0f7406e93410da98",
+    "url": "/JaredRamirezDiaz.github.io/static/media/posterUVAS.e2022059.jpg"
+  }
+]);
